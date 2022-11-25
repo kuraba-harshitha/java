fdacaf067879079df637f4e7c4cc4bac35a528ea
kuraba-harshitha <118730335+kuraba-harshitha@users.noreply.github.com>
2022-11-25 11:59:32 +0530

    InputOutput4.java

---
+++ InputOutput4.java
@@ -0,0 +1,23 @@
+/*Write a program to get the string as input and print it.
+
+Sample Input :
+
+Programmer
+
+Sample Output :
+
+Programmer
+
+ NOTE: Bold letters or words are Input and rest are output statements.*/
+ import java.util.Scanner;
+public class InputOutput4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a;
+        a=sc.nextLine();
+        System.out.println(a);
+        
+    }
+}
