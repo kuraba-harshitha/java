74e29085411e768c64f4ae1653ce11c27585cb44
kuraba-harshitha <118730335+kuraba-harshitha@users.noreply.github.com>
2022-12-01 14:11:32 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,24 @@
+/*Jeevan just started learning Java and C#. At first, he wrote a simple program that was supposed to sum up two small numbers (numbers and their sum fit in a byte), but it didn't work. Vasya was too sad to find out what was wrong. Help him to correct the mistake.
+
+Input:
+
+5
+
+6
+
+Output:
+
+11
+
+*import java.util.Scanner;
+public class add
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println(c);
+    }
+}/
