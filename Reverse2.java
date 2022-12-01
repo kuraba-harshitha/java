0228a2071867e82df6ce8bba8d0bc689c60fa063
kuraba-harshitha <118730335+kuraba-harshitha@users.noreply.github.com>
2022-12-01 13:46:20 +0530

    Reverse2.java

---
+++ Reverse2.java
@@ -0,0 +1,25 @@
+/*Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+Sample Input:
+hello
+Sample Output: 
+olleh*/
+import java.util.*;
+public class Reverse
+{
+   public static void main(String[] args)
+   {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char c;
+        String s1="";
+        for(int i=0;i<s.length();i++)
+        {
+            c=s.charAt(i);
+            s1=c+s1;
+        }
+        System.out.println(s1);
+   }
+}
