d3a5acc6f87676b034cf0eff9bde6d2cbf1d064f
kuraba-harshitha <118730335+kuraba-harshitha@users.noreply.github.com>
2022-12-01 14:29:54 +0530

    duble.java

---
+++ duble.java
@@ -0,0 +1,22 @@
+/*You need to double the integer and return it.
+
+Input:
+
+2
+
+output:
+
+4
+
+*/
+
+import java.util.Scanner;
+public class duble
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(2*n);
+    }
+}
